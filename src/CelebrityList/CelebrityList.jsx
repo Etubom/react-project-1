e3c0f24945aaa844c8942059c451a3236894bdb2
Edem Young <way2young4u@hotmail.com>
2024-06-28 14:07:55 +0100

    Update list on form submit (#1)
    * moved data into own json file
    
    * made architectural changes
    
    * updated celebrity list css
    
    * Moved data activities into parent component

--- src/CelebrityList/CelebrityList.jsx
+++ src/CelebrityList/CelebrityList.jsx
@@ -1,39 +1,133 @@
+import {useState} from "react"
 import CelebrityListItem from "../Components/CelebrityListItem/CelebrityListItem"
-import AddACelebrity from "../Components/AddACelebrity/AddACelebrity"
+import jsonData from "../assets/data.json"
 import styles from './styles.module.css'
 
 const CelebrityList = () => {
-   
+    
+    const [failedValidation, setFailedValidation]=useState(false)
+    const [formData, setFormData] = useState({
+        name : "",
+        job: "",
+        country:"",
+        age: "",
+        netWorth: ""
+    });
 
+    const [data, setData] = useState(jsonData)
 
+    const handleChange = (event) =>{
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    }
 
-    const data = [
-        {
-            "name": "John Mayer",
-            "job": "Singer",
-            "country": "USA",
-            "age": "44",
-            "netWorth": "40M"
-        },
-        {
-            "name": "Dwayne Johnson",
-            "job": "Actor",
-            "country": "USA",
-            "age": "49",
-            "netWorth": "320M"
-        },
-        {
-            "name": "Mike Tyson",
-            "job": "Boxer",
-            "country": "USA",
-            "age": "55",
-            "netWorth": "10M"
+    const checkFormData = (formObject) => {
+        for (let formObjectKey in formObject) {
+            if (formObject[formObjectKey] === ""){
+                return true;
         }
-    ]
+    }
+        return false;
+    }
+
+    const handleSubmit = (event )=> {
+
+        event.preventDefault();
+
+        if(checkFormData(formData)){
+            setFailedValidation(true)
+            return ;
+        }else{
+
+            setData(prevData => [...prevData,formData])
+            setFormData({
+                name : "",
+                job: "",
+                country:"",
+                age: "",
+                netWorth: ""
+            })
+        }
+        
+    }
+
+  
  return (
     <>
         <div className={styles.myForm}>
-            <AddACelebrity/>
+            <form onSubmit={handleSubmit}>
+             <input
+              type="text" 
+              name="name"
+              value={formData.name}
+              placeholder="Name"
+              onChange={handleChange}
+              className={styles.formInput}
+            />
+            <br/>
+            <input
+              type="text" 
+              name="job"
+              value={formData.job }
+              placeholder="Job"
+              onChange={handleChange}
+              className={styles.formInput}
+            />
+            <br/>
+            <input
+              type="text" 
+              name="country"
+              value={formData.country}
+              placeholder="Country"
+              onChange={handleChange}
+              className={styles.formInput}
+
+            />
+            <br/>
+            <input
+              type="text" 
+              name="age"
+              value={formData.age}
+              placeholder="Age"
+              onChange={handleChange}
+              className={styles.formInput}
+
+            />
+            <br/>
+            <input
+              type="text" 
+              name="netWorth"
+              value={formData.netWorth }
+              placeholder="Net Worth"
+              onChange={handleChange}
+              className={styles.formInput}
+
+            />
+             <br/>
+             <div className={styles.submitBtn}>
+                <button type="submit">{"Add User"}</button>
+             </div>
+            {
+            failedValidation && !formData.name && 
+            <p className={styles.formError}>Please insert a name value</p>
+            }
+             {
+            failedValidation && !formData.job && 
+            <p className={styles.formError}>Please insert a job value</p>
+            }
+             {
+            failedValidation && !formData.country && 
+            <p className={styles.formError}>Please insert a country value</p>
+            }
+             {
+            failedValidation && !formData.age &&
+             <p className={styles.formError}>Please insert a age value</p>
+            }
+             {
+            failedValidation && !formData.netWorth && 
+            <p className={styles.formError}>Please insert a net worth value</p>
+            }
+            </form>
         </div>
         <div className={styles.container}>
             {data.map((celebrity, index) => (
