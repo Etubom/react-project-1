a7582fe961e38f47417831b7f73e08617cdb97f5
etubom <way2young4u@hotmail.com>
2024-07-08 22:27:02 +0100

    update display file with context

--- src/Components/CelebrityListDisplay/CelebrityListDisplay.jsx
+++ src/Components/CelebrityListDisplay/CelebrityListDisplay.jsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react'
 import CelebrityListItem from '../CelebrityListItem/CelebrityListItem'
 import styles from './styles.module.css'
+import { CelebrityContext } from '../../Context/CelebrityProvider'
 
-const CelebrityListDisplay = ({ data }) => {
+const CelebrityListDisplay = () => {
+  const { data } = useContext(CelebrityContext)
+  console.log('data within celeb display is ', data)
   return (
     <div className={styles.container}>
-      {data.map((celebrity, index) => (
+      {data?.map((celebrity, index) => (
         <CelebrityListItem key={index} data={celebrity} />
       ))}
     </div>
